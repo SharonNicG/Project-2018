43b04d4ea050bb58ff8067644a3a43b323fd2a57
Sharon <SharonNicG@Sharons-MacBook-Pro.local>
2018-04-23 22:28:12 +0100

    Application of script to backup data set

--- fishers_iris_data_set/opendatafile.py
+++ fishers_iris_data_set/opendatafile.py
@@ -12,3 +12,14 @@ with open('iris.csv') as f:
 
        # outputs requested unpacked arguments as split list  
        print (*line.split(','))
+
+# applies above script to backup data file to check content
+
+# opens CSV file and extracts the data for reading
+with open('iris_bup.csv') as bup:
+    
+    # iterates over each line of data in opened file
+    for line in bup:
+
+       # outputs requested unpacked arguments as split list  
+       print (*line.split(','))
