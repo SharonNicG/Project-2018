b0729d88e65ed8b003995fad7248026fcca4c8c9
Sharon <SharonNicG@Sharons-MacBook-Pro.local>
2018-04-23 22:18:37 +0100

    Created file to open, check and close dataset

---
+++ fishers_iris_data_set/opendatafile.py
@@ -0,0 +1,15 @@
+# Sharon Gibbons, 23-04-2018
+# Iris dataset
+# http://archive.ics.uci.edu/ml/datasets/Iris
+
+# Python script that opens, checks and prints the Iris dataset
+
+# opens data file in current workflow
+f = open('iris.csv')
+
+# Returns True if the file stream can be read from.
+# https://www.programiz.com/python-programming/file-operation
+f.readable()
+
+# closes opened file
+f.close
