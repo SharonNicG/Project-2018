fa2140734dc05fc9d138de0543f0d071b19fe407
SharonNicG <G00364707@gmit.ie>
2018-04-28 15:42:01 +0100

    Summary investigation of data set, confirming number of instances.

---
+++ data_summary.py
@@ -0,0 +1,20 @@
+# Sharon Gibbons, 21-04-2018
+# Summary review of Iris data set
+
+# Load NumPy package
+import numpy as np 
+
+# Load pandas package
+import pandas as pd
+
+# Load csv data file in Python
+datafile = 'fishers_iris_data_set/iris.csv'
+
+# Assign headers to columns
+headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+# Read csv data file with pandas read_csv function
+df = pd.read_csv(datafile, names=headers)
+
+# View instances i.e the number of rows and columns
+print(df.shape)
