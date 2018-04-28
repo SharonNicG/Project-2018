e26a93ff46d2c7ee4c013ab999fe97d35a9eef22
SharonNicG <G00364707@gmit.ie>
2018-04-28 11:28:10 +0100

    Updated output format

--- Python_setup.py
+++ Python_setup.py
@@ -1,18 +1,19 @@
 # Sharon Gibbons, 21-04-2018
 # Check and output details of Python environment
+# https://docs.python.org/3/tutorial/inputoutput.html
 
 # Checks and outputs version number of the Python interpreter
 # https://docs.python.org/2/library/sys.html#sys.version
 import sys
-print(sys.version)
+print('Python version: {}'.format(sys.version))
 
 # Imports module and outputs version details
 # https://pythontips.com/2013/08/28/finding-the-module-version/
 import scipy
-print(scipy.__version__)
+print('SciPy version: {}'.format(scipy.__version__))
 
 import pandas
-print(pandas.__version__)
+print('pandas version: {}'.format(pandas.__version__))
 
 import matplotlib
-print(matplotlib.__version__)
+print('Matplotlib version: {}'.format(matplotlib.__version__))
