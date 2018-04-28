c03ebb44b8724ebe968fb011296f846b1bf1d984
SharonNicG <G00364707@gmit.ie>
2018-04-28 14:44:49 +0100

    Assign headers to columns and test output

--- assign_headers.py
+++ assign_headers.py
@@ -8,8 +8,11 @@ import pandas as pd
 # Load csv data file in Python
 datafile = 'fishers_iris_data_set/iris.csv'
 
-# Read csv data file with pandas read_csv function
-df = pd.read_csv(datafile)
+# Assign headers to columns
+headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 
-# View output of read by pandas
-print(df)
+# Read csv data file with pandas read_csv function
+df = pd.read_csv(datafile, names=headers)
+
+# View column names
+print(df.head())
