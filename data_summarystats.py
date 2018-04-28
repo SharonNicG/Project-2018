3b0638b94fc5d2b631ef4e1f80293167821031bd
SharonNicG <G00364707@gmit.ie>
2018-04-28 16:34:16 +0100

    A summary review of data set: max and min of values in each column

---
+++ data_summarystats.py
@@ -0,0 +1,23 @@
+# Sharon Gibbons, 21-04-2018
+# Summary review of Iris data set
+
+# Load NumPy package
+import numpy as np 
+
+# Load pandas package
+import pandas as pd
+
+# Load csv data file in Python
+datafile = 'fishers_iris_data_set/iris.csv'
+
+# Assign headers to columns
+headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+# Read csv data file with pandas read_csv function
+df = pd.read_csv(datafile, names=headers)
+
+# Identify the lowest value in each column
+print(df.min())
+
+# Identifies the highest value in each column
+print(df.max())
