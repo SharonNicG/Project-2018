773208cd77a2f8d0daa1f9ac6deaddf7d199369b
SharonNicG <G00364707@gmit.ie>
2018-04-29 16:02:37 +0100

    Inclusion of with statement to close file after functions complete

--- data_stats_summary.py
+++ data_stats_summary.py
@@ -7,35 +7,35 @@ import numpy as np
 # Load pandas package
 import pandas as pd
 
-# Load csv data file in Python
-datafile = 'fishers_iris_data_set/iris.csv'
+# Load csv data file in Python using with statement to ensure files closes after functions complete
+with open('fishers_iris_data_set/iris.csv') as datafile:
 
-# Assign headers to columns
-headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    # Assign headers to columns
+    headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 
-# Read csv data file with pandas read_csv function
-df = pd.read_csv(datafile, names=headers)
+    # Read csv data file with pandas read_csv function
+    df = pd.read_csv(datafile, header=None, names=headers)
 
-# Identify the lowest value in each column
-print("The lowest value in each column is: \n{}".format(df.min()))
-print('\n')
+    # Identify the lowest value in each column
+    print("The lowest value in each column is: \n{}".format(df.min()))
+    print('\n')
 
-# Identifies the highest value in each column
-print("The highest value in each column is: \n{}".format(df.max()))
-print('\n')
+    # Identifies the highest value in each column
+    print("The highest value in each column is: \n{}".format(df.max()))
+    print('\n')
 
-# Provides mean/average value of each column
-print("The mean value in each column is: \n{}".format(df.mean()))
-print('\n')
+    # Provides mean/average value of each column
+    print("The mean value in each column is: \n{}".format(df.mean()))
+    print('\n')
 
-# Provides median/midpoint value for each column
-print("The median value in each column is: \n{}".format(df.median()))
-print('\n')
+    # Provides median/midpoint value for each column
+    print("The median value in each column is: \n{}".format(df.median()))
+    print('\n')
 
-# Provides the standard deviation (std) for each column
-print("The standard deviation of the values in each column is: \n{}".format(df.std()))
-print('\n')
+    # Provides the standard deviation (std) for each column
+    print("The standard deviation of the values in each column is: \n{}".format(df.std()))
+    print('\n')
 
-# Provides summary statistics for numerical columns
-# Includes count, min, max, mean, std and percentiles for data in each column
-print("Statistical summary of data \n{}".format(df.describe()))
+    # Provides summary statistics for numerical columns
+    # Includes count, min, max, mean, std and percentiles for data in each column
+    print("Statistical summary of data \n{}".format(df.describe()))
