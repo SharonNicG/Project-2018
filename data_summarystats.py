a10b53d8c2b7c5a25a40e32f08d9026a3b02b199
SharonNicG <G00364707@gmit.ie>
2018-04-29 14:50:42 +0100

    Updated output formatting

--- data_summarystats.py
+++ data_summarystats.py
@@ -18,18 +18,23 @@ df = pd.read_csv(datafile, names=headers)
 
 # Identify the lowest value in each column
 print("The lowest value in each column is: \n{}".format(df.min()))
+print('\n')
 
 # Identifies the highest value in each column
 print("The highest value in each column is: \n{}".format(df.max()))
+print('\n')
 
 # Provides mean/average value of each column
 print("The mean value in each column is: \n{}".format(df.mean()))
+print('\n')
 
 # Provides median/midpoint value for each column
 print("The median value in each column is: \n{}".format(df.median()))
+print('\n')
 
 # Provides the standard deviation (std) for each column
 print("The standard deviation of the values in each column is: \n{}".format(df.std()))
+print('\n')
 
 # Provides summary statistics for numerical columns
 # Includes count, min, max, mean, std and percentiles for data in each column
