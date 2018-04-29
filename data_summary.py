83875392889cba6cf440b67f796253574c1bd20b
SharonNicG <G00364707@gmit.ie>
2018-04-29 15:37:03 +0100

    Inclusion of with statement to close file after functions complete

--- data_summary.py
+++ data_summary.py
@@ -8,27 +8,27 @@ import numpy as np
 # Load pandas package
 import pandas as pd
 
-# Load csv data file in Python
-datafile = 'fishers_iris_data_set/iris.csv'
+# Load csv data file in Python using with statement to ensure files closes after functions complete
+with open('fishers_iris_data_set/iris.csv') as datafile:
 
-# Assign headers to columns
-headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    # Assign headers to columns
+    headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 
-# Read csv data file with pandas read_csv function
-df = pd.read_csv(datafile, header=None, names=headers)
+    # Read csv data file with pandas read_csv function
+    df = pd.read_csv(datafile, header=None, names=headers)
 
-# View first 5 rows and headers 
-print(df.head())
-print('\n')
+    # View first 5 rows and headers 
+    print(df.head())
+    print('\n')
 
-# View instances i.e the number of rows and columns
-print(df.shape)
-print('\n')
+    # View instances i.e the number of rows and columns
+    print(df.shape)
+    print('\n')
 
-# Summary of basic data about Dataframe and it's data
-print(df.info())
-print('\n')
+    # Summary of basic data about Dataframe and it's data
+    print(df.info())
+    print('\n')
 
-# Check for missing attribute values
-print(df.count())
-print('\n')
+    # Check for missing attribute values
+    print(df.count())
+    print('\n')
