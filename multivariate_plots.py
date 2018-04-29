bb4744e4f4599dff76cee15a10170678eedca18c
Sharon <you@example.com>
2018-04-29 20:11:08 +0100

    Create file for data visualisation

---
+++ multivariate_plots.py
@@ -0,0 +1,21 @@
+# Sharon Gibbons, 29-04-2018
+# Visualisation of data analysis on Iris data set
+
+# Load NumPy package
+import numpy as np 
+
+# Load pandas package
+import pandas as pd
+
+# Load libraries for visualisation
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+
+# Load csv data file in Python using with statement to ensure files closes after functions complete
+with open('fishers_iris_data_set/iris.csv') as datafile:
+
+    # Assign headers to columns
+    headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+    # Read csv data file with pandas read_csv function
+    df = pd.read_csv(datafile, header=None, names=headers)
